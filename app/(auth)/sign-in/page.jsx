16a9f06fd46bb53bf8759bc7c19bbae3828465a4
obiito21 <luis.carrasquilla.z@gmail.com>
2025-05-09 15:29:53 -0500

    Implementing Github signIn

--- app/(auth)/sign-in/page.jsx
+++ app/(auth)/sign-in/page.jsx
@@ -1,7 +1,40 @@
+"use client"
+import { Button } from "@/components/ui/button";
 
+import { toast } from "sonner";
+
+
+import ROUTES from "@/app/constants/route";
+import { signIn } from "next-auth/react";
 import Image from "next/image";
 
-function SignIn() {
+function SignIn() {  
+  const handleSignIn = async (provider) => {
+    
+    try{
+      //throw new Error("Not implemented")
+      await signIn(provider, {
+        callbackUrl: ROUTES.HOME,
+        redirect: true
+      })
+    } catch (error) {
+      console.log(error)
+      
+      toast(error instanceof Error 
+        ? error.message
+        : "An error ocurred during sign-in"
+      )
+      /* toast({
+        id: 1,
+        title: "Sign In Failed",
+        description:
+        error instanceof Error 
+          ? error.message
+          : "An error ocurred during sign-in",
+        variant: "destructive"
+      }) */
+    }
+  }
   return (
     <>
     <div className="relative min-h-screen bg-gray-100  min-w-[300px]">
@@ -14,30 +47,33 @@ function SignIn() {
         </div>
 
         <div className="min-w-[300px] mt-12 w-full flex flex-wrap gap-2 items-center justify-center">
-          <div className=" min-w-[100px] w-3/4 flex items-center justify-evenly bg-slate-700 rounded-lg px-4 py-3">
-            <Image
-              src={"/icons/github.svg"}
-              alt="Github Logo"
-              width={30}
-              height={30}
-              className="invert-colors mr-2.5 object-contain"
-            />
-            <button className="rounded-full w-1/2 ">
-              <span> Log in with Github</span>
-            </button>
+          <div className="mt-5 flex-wrap gap-2 min-w-[100px] w-3/4 flex items-center justify-evenly bg-slate-700 rounded-lg px-4 py-3">
+            <Button className='' onClick={() => handleSignIn("github")}>
+              <Image
+                src={"/icons/github.svg"}
+                alt="Github Logo"
+                width={30}
+                height={30}
+                className="invert-colors mr-2.5 object-contain"
+              />
+            
+                <span> Log in with Github</span>
+            </Button>
+            
           </div>
           
-          <div className="min-w-[100px] w-3/4 flex items-center justify-evenly  bg-slate-700 rounded-lg px-4 py-3">
-            <Image
+          <div className="flex-wrap gap-2 min-w-[100px] w-3/4 flex items-center justify-evenly bg-slate-700 rounded-lg px-4 py-3">
+             <Button className=''>
+                <Image
               src={"/icons/google.svg"}
               alt="Github Logo"
               width={30}
               height={30}
               className="invert-colors mr-2.5 object-contain"
             />
-            <button className="rounded-full w-1/2 ">
               <span> Log in with Google</span>
-            </button>
+             </Button>
+
           </div>
         </div>
       </div>
